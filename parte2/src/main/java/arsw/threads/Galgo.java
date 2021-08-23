166bcbcadda93d9f20f765683d9565ae8e1a0580
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-08-23 16:38:36 -0500

    posible solucion Parte III (puestos repetidos)

--- parte2/src/main/java/arsw/threads/Galgo.java
+++ parte2/src/main/java/arsw/threads/Galgo.java
@@ -29,12 +29,16 @@ public class Galgo extends Thread {
 			carril.displayPasos(paso);
 			if (paso == carril.size()) {
 				carril.finish();
-				int ubicacion=regl.getUltimaPosicionAlcanzada();
-				regl.setUltimaPosicionAlcanzada(ubicacion+1);
-				System.out.println("El galgo "+this.getName()+" llego en la posicion "+ubicacion);
-				if (ubicacion==1){
-					regl.setGanador(this.getName());
+
+				synchronized ( regl ){
+					int ubicacion=regl.getUltimaPosicionAlcanzada();
+					regl.setUltimaPosicionAlcanzada(ubicacion+1);
+					System.out.println("El galgo "+this.getName()+" llego en la posicion "+ubicacion);
+					if (ubicacion==1){
+						regl.setGanador(this.getName());
+					}
 				}
+
 			}
 			// Region critica
 			synchronized ( this ){
--- parte2/src/main/java/arsw/threads/RegistroLlegada.java
+++ parte2/src/main/java/arsw/threads/RegistroLlegada.java
@@ -14,11 +14,11 @@ public class RegistroLlegada {
 		this.ganador = ganador;
 	}
 
-	public int getUltimaPosicionAlcanzada() {
+	public synchronized int getUltimaPosicionAlcanzada() {
 		return ultimaPosicionAlcanzada;
 	}
 
-	public void setUltimaPosicionAlcanzada(int ultimaPosicionAlcanzada) {
+	public synchronized void setUltimaPosicionAlcanzada(int ultimaPosicionAlcanzada) {
 		this.ultimaPosicionAlcanzada = ultimaPosicionAlcanzada;
 	}
 
